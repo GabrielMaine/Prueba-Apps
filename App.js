6e8ce1c76ad70ee11ae00f4b56f9793f87424852
Gabriel Maine <gabrielmaine14@gmail.com>
2024-05-26 16:47:24 -0300

    Feat: clase 4

--- App.js
+++ App.js
@@ -1,12 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, Modal, FlatList } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [text, setText] = useState("")
+  const [tareas, setTareas] = useState([{id:1,value:"Darle mimitos a Angie"}])
+
+  const onHandlerChangeItem = (t) => setText(t)
+  const addItem = () => {
+    setTareas(tareasActuales => [
+      ...tareasActuales,
+      {id: Math.random().toString(), value:text}
+    ])
+    setText("")
+  }
+  const borrarTarea = (id) => {
+    setTareas(tareasActuales => tareasActuales.filter(tarea => tarea.id !== id));
+  }
+
+  const renderItem = ({ item }) => (
+    <View>
+      <Text>{item.value}</Text>
+      <Button title="Borrar" onPress={()=>borrarTarea(item.id)}/>
     </View>
+  );
+
+  return (
+    <>
+    <View style={styles.container}>
+      <Text>Les dsapdosar agua</Text>
+      <StatusBar style="auto" />
+      <View>
+        <TextInput 
+          placeholder='Pasear al perro?'
+          onChangeText={onHandlerChangeItem}
+          value={text}
+          />
+        <Button title="Agregar" onPress={addItem}/>
+      </View>
+      <FlatList
+        data={tareas}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+    </>
   );
 }
 
@@ -17,4 +55,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  boton:{
+    backgroundColor: 'aqua',
+  },
+  renglon:{
+    flexDirection: 'row',
+    backgroundColor: 'red',
+    padding: 10,
+  }
 });
